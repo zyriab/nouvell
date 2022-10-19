91a009170304dc40d4765bf93c9cdd440aa90576
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-20 00:58:58 +0200

    feature: format given products to standardize category and display name

--- src/utils/database/filterNonExistingProducts.utils.ts
+++ src/utils/database/filterNonExistingProducts.utils.ts
@@ -1,4 +1,5 @@
-import { Product } from '../../definitions/generated/graphql';
+import { Product } from '../../definitions/types';
+import { Product as ProductModel } from '../../models/models';
 import isProductExisting from './isProductExisting.utils';
 
 export default async function filterNonExistingProducts(prodsArr: Product[]) {
@@ -6,11 +7,26 @@ export default async function filterNonExistingProducts(prodsArr: Product[]) {
     prodsArr.map((p) => isProductExisting(p))
   );
 
-  const filteredProds = prodsArr.filter((p, i) => prodsExist[i]);
+  let filteredProds: Product[] = prodsArr.filter((p, i) => prodsExist[i]);
 
   if (filteredProds.length === 0) {
     throw new Error('No products left after filtering non-existing ones!');
   }
+
+  let tmpProd: Product;
+  let tmpDoc: Product;
+
+  filteredProds = await Promise.all(
+    filteredProds.map(async (p) => {
+      tmpProd = { ...p };
+      tmpDoc = (await ProductModel.findOne({ name: p.name }))!;
+
+      tmpProd.category = tmpDoc.category;
+      tmpProd.displayName = tmpDoc.displayName;
+      
+      return tmpProd;
+    })
+  );
 
   return filteredProds;
 }
