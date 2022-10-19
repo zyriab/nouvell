4d2460debf32d16bc48fb7c184af84ccf17a4ce2
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-20 01:40:45 +0200

    chore: removed useless Map entry

--- src/helpers/constants.help.ts
+++ src/helpers/constants.help.ts
@@ -2,4 +2,3 @@
 
 export const AUTH0_DOMAINS = new Map<string, string>();
 AUTH0_DOMAINS.set('default', 'metaoist-dsgn.eu.auth0.com');
-AUTH0_DOMAINS.set('shopicsv-dev', 'shopicsv-dev.eu.auth0.com');
