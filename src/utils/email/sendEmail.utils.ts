e802b7e42efc35247fbdc83654389166006775be
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-28 21:19:37 +0200

    feature: made use of the aynchronous version of nomailer's sendMail fn

--- src/utils/email/sendEmail.utils.ts
+++ src/utils/email/sendEmail.utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import mailer from 'nodemailer';
 import {
   EmailData,
@@ -13,31 +14,32 @@ interface SendEmailArgs {
   emailData: EmailData;
 }
 
-export default function sendEmail(args: SendEmailArgs) {
-  const transporter = mailer.createTransport({
-    host: args.sender.host,
-    port: args.sender.port,
-    secure: true,
-    auth: {
-      user: args.sender.email,
-      // FIXME: hash is not possible but storing encrypted password is bad practice!
-      pass: <string>decryptAES(args.sender.pass!),
-    },
-  });
+export default async function sendEmail(args: SendEmailArgs) {
+  try {
+    const transporter = mailer.createTransport({
+      host: args.sender.host,
+      port: args.sender.port,
+      secure: true,
+      auth: {
+        user: args.sender.email,
+        // FIXME: hash is not possible but storing encrypted password is bad practice!
+        pass: <string>decryptAES(args.sender.pass!),
+      },
+    });
 
-  transporter.sendMail(
-    {
+    const info = await transporter.sendMail({
       from: `${args.sender.displayName} ${args.sender.email}`,
       to: args.recipients[0].email,
       subject: args.emailData.subject,
       text: <string | undefined>args.emailData.textBody,
       html: <string | undefined>args.emailData.htmlBody,
       amp: <string | undefined>args.emailData.ampBody,
-    },
-    (err, info) => {
-      if (err) throw err;
-      // TODO: check for non-delivered emails and do smth about it :)
-      console.log(info);
-    }
-  );
+    });
+
+    // TODO: check for non-delivered emails and do smth about it :)
+    console.log(info);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 }
