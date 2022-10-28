0e3ef0baaf01c233c7de1fa5af3a7aedd610a28c
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-29 00:41:38 +0200

    fix: formatting argument

--- src/utils/database/getProduct.utils.ts
+++ src/utils/database/getProduct.utils.ts
@@ -2,7 +2,7 @@ import { Product as ProductModel } from '../../models/models';
 import { Product } from '../../definitions/types';
 
 export default async function getProduct(name: string): Promise<Product> {
-  const product = await ProductModel.findOne({ name });
+  const product = await ProductModel.findOne({ name: name.toLowerCase() });
 
   if (product == null) {
     throw new Error(`Product ${name} not found`);
