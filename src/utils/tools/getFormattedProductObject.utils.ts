2273c9b486b643f265b5c74ccf4f98e17eda518b
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-28 21:18:45 +0200

    chore: added TODO note

--- src/utils/tools/getFormattedProductObject.utils.ts
+++ src/utils/tools/getFormattedProductObject.utils.ts
@@ -1,5 +1,6 @@
 import { Product } from '../../definitions/generated/graphql';
 
+// TODO: add a function to get the proper product name, category and displayName from the DB and use this one only for adding products
 export default function getFormattedProductObject(product: Product) {
   return {
     name: product.name.toLowerCase(),
