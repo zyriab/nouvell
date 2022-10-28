dc8dafd895f77c893ab20fa01bd4868b38530567
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-28 21:17:56 +0200

    fix: fixed input args type

--- src/utils/database/filterNonExistingProducts.utils.ts
+++ src/utils/database/filterNonExistingProducts.utils.ts
@@ -1,13 +1,18 @@
 import { Product } from '../../definitions/types';
 import { Product as ProductModel } from '../../models/models';
 import isProductExisting from './isProductExisting.utils';
+import { ProductInput } from '../../definitions/generated/graphql';
 
-export default async function filterNonExistingProducts(prodsArr: Product[]) {
+export default async function filterNonExistingProducts(
+  prodsArr: Product[] | ProductInput[]
+): Promise<Product[]> {
   const prodsExist: boolean[] = await Promise.all(
     prodsArr.map((p) => isProductExisting(p))
   );
 
-  let filteredProds: Product[] = prodsArr.filter((p, i) => prodsExist[i]);
+  let filteredProds: Product[] | ProductInput[] = prodsArr.filter(
+    (p, i) => prodsExist[i]
+  );
 
   if (filteredProds.length === 0) {
     throw new Error('No products left after filtering non-existing ones!');
@@ -18,7 +23,7 @@ export default async function filterNonExistingProducts(prodsArr: Product[]) {
 
   filteredProds = await Promise.all(
     filteredProds.map(async (p) => {
-      tmpProd = { ...p };
+      tmpProd = { ...p, category: '', displayName: '' };
       tmpDoc = (await ProductModel.findOne({ name: p.name }))!;
 
       tmpProd.category = tmpDoc.category;
@@ -28,5 +33,5 @@ export default async function filterNonExistingProducts(prodsArr: Product[]) {
     })
   );
 
-  return filteredProds;
+  return <Product[]>filteredProds;
 }
--- src/utils/database/isProductExisting.utils.ts
+++ src/utils/database/isProductExisting.utils.ts
@@ -1,7 +1,7 @@
 import { Product as ProductModel } from '../../models/models';
-import { Product } from '../../definitions/generated/graphql';
+import { Product, ProductInput } from '../../definitions/generated/graphql';
 
-export default async function isProductExisting(product: Product) {
+export default async function isProductExisting(product: Product | ProductInput) {
   return (
     (await ProductModel.exists({ name: product.name.toLowerCase() })) != null
   );
